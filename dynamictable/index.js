ad09842b35283fdb12cf666d73f58791a0217c14
younes <youn.henni@icloud.com>
2024-01-01 12:13:50 +0100

    Dynamic data loading in table

--- dynamictable/index.js
+++ dynamictable/index.js
@@ -1,3 +1,9 @@
+function getData() {
+  return [...Array(100)].map((_, i) =>
+    fetch("http://193.70.113.26:3000/" + (i + 1)).then((res) => res.json())
+  );
+}
+
 document.getElementById("form").addEventListener("submit", (e) => {
   e.preventDefault();
   const firstName = e.target[0].value;
@@ -21,4 +27,25 @@ document.getElementById("form").addEventListener("submit", (e) => {
   tr.appendChild(td4);
   tableBody.appendChild(tr);
   e.target.reset();
+});
+
+getData().map(async (promise) => {
+  const data = await promise;
+  data.map((row) => {
+    const tableBody = document.querySelector("#myTable > tbody");
+    const tr = document.createElement("tr");
+    const td1 = document.createElement("td");
+    td1.innerText = row.first_name;
+    const td2 = document.createElement("td");
+    td2.innerText = row.last_name;
+    const td3 = document.createElement("td");
+    td3.innerText = row.email;
+    const td4 = document.createElement("td");
+    td4.innerText = row.gender;
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);
+    tableBody.appendChild(tr);
+  });
 });
